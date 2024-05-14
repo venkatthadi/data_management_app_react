29989810df4c25f13c72fa66d380cb2d1e72bef6
venkatthadi <venkat@mobicip.com>
2024-05-14 19:10:41 +0530

    accounts - add account

--- src/components/AccountContainer.jsx
+++ src/components/AccountContainer.jsx
@@ -1,14 +1,59 @@
-import { useContext } from "react";
-import { AccountsContext } from "../utils/contexts/accountsContext";
+import { useContext, useEffect, useState } from "react";
+import { AccountsContext, userApiUrl } from "../utils/contexts/accountsContext";
 import { AccountContentButtons } from "./AccountContentButtons";
+import { useFetchAccounts } from "../utils/hooks/accounts/useFetchAccounts";
 
 
 export function AccountContainer() {
     
     const accountsContextData = useContext(AccountsContext);
+    const [ name, setName ] = useState("");
 
     return(
         <div className="container">
+            <br/>
+            <form
+                onSubmit={(e) => {
+                        
+                        // e.preventDefault();
+
+                        if(name) {
+                            fetch(userApiUrl, {
+                                method: "POST", // POST req
+                                body: JSON.stringify({
+                                    name: name,
+                                }),
+                                headers: {
+                                    'Content-type': 'application/json; charset=UTF-8',
+                                }
+                            })
+                            .then((response) => response.json())
+                            .then((data) => {
+                                console.log("Success");
+                                console.log(data);
+                            }).catch((err) => console.log(err));
+
+                            setSync((sync) => !sync);
+                        }
+
+                    }
+                }
+            >
+                <div>
+                    <label htmlFor="name">Name: </label>  
+                    <input 
+                        id="name" 
+                        name="name"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                </div>
+                <button>
+                    Add account
+                </button>
+            </form>
         <table className="table table-hover table-striped table-bordered">
             <thead className="thead-dark">
                 <tr>
