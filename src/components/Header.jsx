50a0af2cb5820cccab2d768f6bb2e8c57061d8d5
venkatthadi <venkat@mobicip.com>
2024-05-14 15:46:36 +0530

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
     return(
-        <div id="header">
-            Header
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link to="/" className="btn btn-secondary">Home</Link>
+            <div className="navbar">
+                <ul className="nav navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link to="/users" className="btn btn-secondary">Accounts</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
