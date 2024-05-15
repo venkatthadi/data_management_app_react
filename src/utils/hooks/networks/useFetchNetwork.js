abc0ab4fcdf0bda3bce87092c181d0b0ac94ff2d
venkatthadi <venkat@mobicip.com>
2024-05-15 15:14:13 +0530

    added files for remaining components

---
+++ src/utils/hooks/networks/useFetchNetwork.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { userApiUrl } from "../../contexts/accountsContext";
+
+export function useFetchAccount(accountId) {
+
+    const [ accountData, setAccountData ] = useState({});
+    const [ loading, setLoading ] = useState(false);
+    const [ errors, setErrors ] = useState();
+
+    useEffect(() => {
+
+        const controller = new AbortController();
+
+        setLoading(true);
+        fetch(`${userApiUrl}/${accountId}`, { signal: controller.signal, })
+        .then((response) => response.json())
+        .then((data) => {
+            setAccountData(data);
+            setErrors(undefined);
+        })
+        .catch((err) => {
+            setErrors(err);
+        })
+        .finally(() => {
+            setTimeout(() => {
+                setLoading(false)
+            }, 2000);
+        });
+
+        return () => {
+            controller.abort();
+            setTimeout(() => {
+                setLoading(false)
+            }, 2000);
+        };
+    }, [accountId]);
+
+    return { account: accountData, loading, errors };
+}
