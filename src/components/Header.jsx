7f946232f757f5322fb860244077aa1743b91b91
venkatthadi <venkat@mobicip.com>
2024-05-16 10:51:13 +0530

    Navbar changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,22 @@ export function Header() {
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <Link to="/" className="navbar-brand">Home</Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link to="/accounts" className="btn btn-secondary">Accounts</Link>
+                    <li className="nav-item active">
+                        <Link to="/accounts" className="nav-link">Accounts</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link to="/networks" className="btn btn-secondary">Networks</Link>
+                    <li className="nav-item active">
+                        <Link to="/networks" className="nav-link">Networks</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link to="/schools" className="btn btn-secondary">Schools</Link>
+                    <li className="nav-item active">
+                        <Link to="/schools" className="nav-link">Schools</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link to="/usertypes" className="btn btn-secondary">Usertypes</Link>
+                    <li className="nav-item active">
+                        <Link to="/usertypes" className="nav-link">Usertypes</Link>
                     </li>
-                    <li className="nav-item">
-                        <Link to="/users" className="btn btn-secondary">Users</Link>
+                    <li className="nav-item active">
+                        <Link to="/users" className="nav-link">Users</Link>
                     </li>
                 </ul>
             </div>
