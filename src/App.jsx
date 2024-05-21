1105b6fa7373f90abaed5d69ad2b888fdc06cf18
venkatthadi <venkat@mobicip.com>
2024-05-21 18:03:25 +0530

    home screen changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Header } from "./components/Header";
-import { useState, useContext } from "react";
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { useAuth } from "./utils/contexts/authContext";
 
 export default function App() {
+
+    const { isAuth } = useAuth();
 
     return(
         <div>
             <Header />
+            <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Welcome to Data Management System</h1>
+                { !isAuth && <p className="lead">Please login to continue</p> }
+            </div>
+            </div>
         </div>
     );
 }
